9a55dd6c327b745bb144d7a667ff29f6876b06bc
roman abishev <r.abishev@bi.zone>
2022-09-06 19:07:21 +0300

    delete test_for_work

--- conftest.py
+++ conftest.py
@@ -10,8 +10,3 @@ def open_browser():
     browser.config.hold_browser_open = True
 
 
-@pytest.fixture()
-def open_browser_work():
-    browser.open('http://feature5.def.k8s.dev.bi.zone/app/#/login')
-    yield
-    browser.config.hold_browser_open = True
